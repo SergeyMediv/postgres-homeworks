2dc98ba31c008f7e84eb5805d6d2162b2b1f5253
SergeyMediv <bochkarev.s.e@yandex.ru>
2024-02-04 19:17:32 +0300

    hw_3_fix

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -2,10 +2,12 @@
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
 
-SELECT company_name AS customer, CONCAT(employees.first_name, ' ' ,employees.last_name) AS employee
+SELECT customers.company_name AS customer, CONCAT(employees.first_name, ' ', employees.last_name) AS employee
 FROM customers
-JOIN employees USING(city)
-WHERE customer_id in (SELECT customer_id FROM orders WHERE ship_via='2') AND city='London'
+JOIN orders USING (customer_id)
+JOIN employees USING (employee_id)
+JOIN shippers ON orders.ship_via = shippers.shipper_id
+WHERE customers.city = 'London' AND employees.city = 'London' AND shippers.company_name = 'United Package'
 
 
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
